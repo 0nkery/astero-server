74fd2672f4ff8841d2bb79e8d56ee682c368d979
Dmitry Shlagoff <richyannet@yandex.ru>
2017-12-12 16:19:54 +0700

    Initial commit for 'sector' app

---
+++ apps/sector/lib/sector.ex
@@ -0,0 +1,18 @@
+defmodule Sector do
+  @moduledoc """
+  Documentation for Sector.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sector.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/sector/test/sector_test.exs
@@ -0,0 +1,8 @@
+defmodule SectorTest do
+  use ExUnit.Case
+  doctest Sector
+
+  test "greets the world" do
+    assert Sector.hello() == :world
+  end
+end
