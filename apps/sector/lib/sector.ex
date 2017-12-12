757de5246730a4985cd21da7fc8365137f515e0c
Dmitry Shlagoff <richyannet@yandex.ru>
2017-12-12 16:59:15 +0700

    Initial Sector process

--- apps/sector/lib/sector.ex
+++ apps/sector/lib/sector.ex
@@ -1,3 +1,22 @@
-defmodule Sector do
+require Logger
 
+defmodule Sector.State do
+  defstruct players: %{}
+end
+
+defmodule Sector do
+  use GenServer
+
+  alias Sector.State
+
+  # Client
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  # Server
+  def init(:ok) do
+    Logger.info("Started #{__MODULE__}")
+    {:ok, %State{}}
+  end
 end
