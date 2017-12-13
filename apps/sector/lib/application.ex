201f924146943595336e6dce64de28a0b187147d
Dmitry Shlagoff <richyannet@yandex.ru>
2017-12-13 10:23:01 +0700

    Dep on 'lobby' app + actually starting Sector server

--- apps/sector/lib/application.ex
+++ apps/sector/lib/application.ex
@@ -8,8 +8,7 @@ defmodule Sector.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Sector.Worker.start_link(arg)
-      # {Sector.Worker, arg},
+      {Sector, name: Sector},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- apps/sector/lib/sector.ex
+++ apps/sector/lib/sector.ex
@@ -14,9 +14,29 @@ defmodule Sector do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
+  def player_joined(conn, player_id, nickname) do
+    GenServer.cast(Sector, {:joined, conn, player_id, nickname})
+  end
+
   # Server
   def init(:ok) do
     Logger.info("Started #{__MODULE__}")
     {:ok, %State{}}
+  end
+
+  def handle_cast(msg, state) do
+    case msg do
+      {:joined, conn, player_id, nickname} ->
+        # TODO: assign coordinates
+
+        ack = Lobby.Msg.ack(player_id)
+        Lobby.Connection.send(conn, ack)
+
+        player_joined = Lobby.Msg.player_joined(player_id, nickname)
+        except = conn
+        Lobby.broadcast(player_joined, except)
+
+        {:noreply, state}
+    end
   end
 end
